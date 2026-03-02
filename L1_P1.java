7783b31ad33cb061e644b4042aed4d50c85de82d
Kanika Arora <kanika648arora@gmail.com>
2026-03-02 21:16:54 +0530

    Add files via upload

---
+++ L1_P1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class L1_P1
+{
+    public static void main (String args[])
+    {
+        /*Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
+I/P => NONE
+O/P => Harry's age in 2024 is ___ */
+        Scanner sc= new Scanner (System.in);
+        int age;
+        age = 2024-2000;
+        System.out.println ("Harry's age in 2024 is " + age);
+    }
+}
